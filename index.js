cb25931b7e0bb3f41e5dda95492b6b1ae0d8dfa1
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-04 05:57:25 +0300

    add res

--- index.js
+++ index.js
@@ -3,4 +3,26 @@
 //http
 
 var http = require("http"); // node modules => http, fs ,os
-var server = http.createServer((req, res) => {});
+var fs = require("fs"); // node modules
+var server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.readFile("index.html", (err, html) => {
+      res.write(html);
+      res.end();
+    });
+  } else if (req.url == "/urunler") {
+    fs.readFile("urunler.html", (err, html) => {
+      res.write(html);
+      res.end();
+    });
+  } else {
+    fs.readFile("404.html", (err, html) => {
+      res.write(html);
+      res.end();
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("node.js server at port 3000");
+});
