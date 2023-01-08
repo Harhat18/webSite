ba2ae67977ab4a266607530a48feada0f4aaf111
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-08 22:19:06 +0300

    add css

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 const data = [
   {
     id: 1,
     name: "iphone 14",
     price: "2000",
     isActive: true,
+    imageUrl: "1.jpeg",
   },
   {
     id: 2,
     name: "iphone 15",
     price: "3000",
     isActive: false,
+    imageUrl: "2.jpeg",
   },
   {
     id: 3,
     name: "iphone 16",
     price: "4000",
     isActive: true,
+    imageUrl: "3.jpeg",
   },
 ];
 
 //routes
 
 app.use("/products/:id", function (req, res) {
-  res.render("product-details");
+  const urun = data.find((u) => u.id == req.params.id);
+  res.render("product-details", urun);
 });
 
 app.use("/products", function (req, res) {
